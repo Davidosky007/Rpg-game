4397fef960e5228c97c4da5a77f1d6a0e443ed65
David Bassey <davidosky1@gmail.com>
2021-04-02 14:08:30 +0100

    Add webpack and linter files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function component() {
+  const element = document.createElement('div');
+  element.innerHTML = `<h1>Hello World</h1>`;
+  return element;
+}
+
+document.body.appendChild(component());
