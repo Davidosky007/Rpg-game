d19e73c6e0a2a635a2c87130d82863757a28917f
David Bassey <davidosky1@gmail.com>
2021-04-02 16:41:04 +0100

    Add phaser config file

---
+++ src/configs/config.js
@@ -0,0 +1,23 @@
+/* eslint-disable linebreak-style */
+import Phaser from 'phaser';
+
+export default {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+      gravity: {
+        y: 0,
+      },
+    },
+  },
+  pixelArt: true,
+  roundPixels: true,
+  dom: {
+    createContainer: true,
+  },
+};
