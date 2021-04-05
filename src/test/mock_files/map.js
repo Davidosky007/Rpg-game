e803f39bed7ff77c51226ab356dd1a7d65a264bd
David Bassey <davidosky1@gmail.com>
2021-04-05 11:27:48 +0100

    Create mock files for testing

---
+++ src/test/mock_files/map.js
@@ -0,0 +1,9 @@
+export default class Map {
+  constructor(scene, key, titleSetName, bgLayerName, blockedLayerName) {
+    this.scene = scene;
+    this.key = key;
+    this.titleSetName =  titleSetName;
+    this.bgLayerName = bgLayerName;
+    this.blockedLayerName = blockedLayerName;
+  }
+}
