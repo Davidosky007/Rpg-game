e174604afed812c010d08c313a5ef28f4339f2f3
David Bassey <davidosky1@gmail.com>
2021-04-06 10:13:26 +0100

    Add style.css file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import './style.css';
 import GameScene from './scenes/gameScene';
 import BootScene from './scenes/bootScene';
 import PreloaderScene from './scenes/preloaderScene';
